5bd4ec808803a3df659a5e67bc5b4471c27f0f92
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-03-26 14:42:02 -0600

    Fixed Html and CSS

--- src/App.js
+++ src/App.js
@@ -1,87 +1,35 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
-
-function Weather({city, temperature}) {
-
-
-  return (
-    <div>
-      <input type="text" id="city" placeholder="Put City here"></input>
-      <table class="dataTable">
-        <tr>
-          <tr>
-            <th>{city}</th>
-          </tr>
-          <td>Sunday</td>
-          <td>Monday</td>
-          <td>Tuesday</td>
-          <td>Wednesday</td>
-          <td>Thursday</td>
-          <td>Friday</td>
-          <td>Saturday</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td><p>{temperature}</p></td>
-          <td><p>{temperature}</p></td>
-          <td><p>{temperature}</p></td>
-          <td><p>{temperature}</p></td>
-          <td><p>{temperature}</p></td>
-          <td><p>{temperature}</p></td>
-          <td><p>{temperature}</p></td>
-        </tr>
-      </table>
-    </div>
-  )
-}
-
+import { useEffect, useCase } from React;
 
 
 function App() {
 
-  let [coordinates, setCoordinates] = useState([]);
-
-  useEffect(() => {
-    async function getCoordinates(){
-      const resp = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Tokyo&appid=2e71c8fc353c994493d1f4bd9575e4b1');
-      const data = await resp.json()
-
-      console.log(data);
-
-      const dataArray = data.toString.split(',');
-
-      console.log(dataArray)
-
-      setCoordinates(data)
-    }
-
-    getCoordinates()
-
-  }, [])
-
-  let [weather, setWeather] = useState([]);
-
-  useEffect(() => {
-    async function getWeather(){
-      const resp2 = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=35.652832&lon=139.839478&appid=2e71c8fc353c994493d1f4bd9575e4b1');
-      const data2 = await resp2.json()
-
-      console.log(data2)
-
-      setWeather(data2)
-
-    } 
-
-    getWeather()
-
-  }, [])
-
   return (
-    <div className="App">
-    {weather = (weather => (
-      <Weather city='Tokyo' temperature={weather.daily.temp.day} />
-      ))}
-    </div>    
+  <>
+    <header>
+      <h1>What is the weather like in Utah?</h1>
+    </header>
+    <main>
+    <form>
+      <input type="text" placeholder="What city do you want?" />
+      <button>Get Weather!</button>
+      <button>Change Temperature unit</button>
+    </form>
+    <h2>The Weather in Orem, Utah</h2>
+    <h3>Currently: 55°F</h3>
+    <h4>7-day Forecast</h4>
+        <h5>Sunday: 69°F</h5>
+        <h5>Monday: 72°F</h5>
+        <h5>Tuesday: 72°F</h5>
+        <h5>Wednesday: 56°F</h5>
+        <h5>Thursday: 53°F</h5>
+        <h5>Friday: 50°F</h5>
+        <h5>Saturday: 50°F</h5>
+  </main>
+  <footer>
+    Page created by Richard Stacey
+  </footer>
+  </>
   );
 }
 
