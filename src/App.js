8b47867b5d98db48f20a32b66dc4a8283f587a82
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-03-26 18:03:03 -0600

    Imported UseEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-//import { useEffect, useCase } from React;
-
+import { useEffect, useCase } from 'react';
 
 function App() {
 
@@ -15,16 +14,16 @@ function App() {
       <button>Get Weather!</button>
       <button>Change Temperature unit</button>
     </form>
-    <h2>The Weather in Orem, Utah</h2>
-    <h3>Currently: 55°F</h3>
+    <h2>The Weather in , Utah</h2>
+    <h3>Currently: </h3>
     <h4>7-day Forecast</h4>
-        <h5>Sunday: 69°F</h5>
-        <h5>Monday: 72°F</h5>
-        <h5>Tuesday: 72°F</h5>
-        <h5>Wednesday: 56°F</h5>
-        <h5>Thursday: 53°F</h5>
-        <h5>Friday: 50°F</h5>
-        <h5>Saturday: 50°F</h5>
+        <h5>Sunday: </h5>
+        <h5>Monday: </h5>
+        <h5>Tuesday: </h5>
+        <h5>Wednesday: </h5>
+        <h5>Thursday: </h5>
+        <h5>Friday: </h5>
+        <h5>Saturday: </h5>
   </main>
   <footer>
     Page created by Richard Stacey
